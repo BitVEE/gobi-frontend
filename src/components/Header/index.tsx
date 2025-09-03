45327277c696858e56c9c917c2387da1fb59cdd7
晶晶王 <15713716017@163.com>
2025-09-03 16:50:53 +0800

    update logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -69,7 +69,7 @@ const Header = () => {
         <header className={styles.header} id='site-header'>
             <div className={styles.container}>
                 <Link href="/" onClick={() => { router.push('/') }}  >
-                    <Image width={150} height={58} className={styles.logo} src="/images/logo.png" alt="GOBI" />
+                    <Image width={900} height={227} className={styles.logo} src="/images/logo.png" alt="GOBI" />
                 </Link>
                 <div className={styles.mobileMenuButton} onClick={toggleMobileMenu}>
                     <Image width={30} height={30} className={styles.mobileMenuIcon} src={isMobileMenuOpen ? "/images/icons/close.svg" : "/images/icons/menu.svg"} alt="menu" />
