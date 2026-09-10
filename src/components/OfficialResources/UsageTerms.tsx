29be41c1c96a4b898e96889c204c56ababca0442
Xavier-Gao <1184072847@qq.com>
2026-09-10 16:23:57 +0800

    feat:add  protocol of using resources

--- src/components/OfficialResources/UsageTerms.tsx
+++ src/components/OfficialResources/UsageTerms.tsx
@@ -2,11 +2,12 @@ import { useTranslation } from 'next-i18next';
 import styles from './resources.module.scss';
 
 export default function UsageTerms() {
-  const { t } = useTranslation('common', { keyPrefix: 'resources' });
+  const { t, i18n } = useTranslation('common', { keyPrefix: 'resources' });
   return (
     <button
       type="button"
       className={styles.termsLink}
+      onClick={() => window.open(`/protocol/青戈赛官方物料使用条款${i18n.language === 'zh' ? '' : '_EN'}.pdf`, '_blank')}
     >
       {t('terms')}
     </button>
